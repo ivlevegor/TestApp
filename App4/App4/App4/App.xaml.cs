75ab397735c7c56f5510c7ae2c1a3f00fcbc65ab
ivlevegor <40693570+ivlevegor@users.noreply.github.com>
2018-07-02 17:33:57 +0300

    Add files via upload

---
+++ App4/App4/App4/App.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace App4
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            AppCenter.Start("android=c1566bda-2abc-4bd0-b848-d33458aa1a49;", typeof(Analytics), typeof(Crashes));
+            // Handle when your app starts
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
